3505ab217c161a4965d32bf717818076dda8e3bf
Francis San Filippo <fsanfilipp@vailsys.com>
2019-10-28 11:03:16 -0500

    freeclimb code changes

--- listQueues.js
+++ listQueues.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
-const persephonySDK = require('@persephony/sdk')
+const freeclimbSDK = require('@freeclimb/sdk')
 
 const accountId = process.env.ACCOUNT_ID
 const authToken = process.env.AUTH_TOKEN
-// your Persephony API key (available in the Dashboard) - be sure to set up environment variables to store these values
-const persephony = persephonySDK(accountId, authToken)
+// your freeclimb API key (available in the Dashboard) - be sure to set up environment variables to store these values
+const freeclimb = freeclimbSDK(accountId, authToken)
 
 getQueues().then(queues => {
   // Use Queues
@@ -18,13 +18,13 @@ async function getQueues() {
   // Create array to store all queues
   const queues = []
   // Invoke GET method to retrieve initial list of queues information
-  const first = await persephony.api.queues.getList()
+  const first = await freeclimb.api.queues.getList()
   queues.push(...first.queues)
   // Get Uri for next page
   let nextPageUri = first.nextPageUri
   // Retrieve entire queues list 
   while (nextPageUri) {
-    const nextPage = await persephony.api.queues.getNextPage(nextPageUri)
+    const nextPage = await freeclimb.api.queues.getNextPage(nextPageUri)
     queues.push(...nextPage.queues)
     nextPageUri = nextPage.nextPageUri
   }
